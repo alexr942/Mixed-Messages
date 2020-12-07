8d47b66007b45dcda3e1d6db531d0df2cf591d88
alexr942 <alexrobertson518@gmail.com>
2020-12-07 16:33:09 +0000

    j

--- mixed messages.js
+++ mixed messages.js
@@ -1,17 +1,15 @@
 // this application will show random messages everytime it is loaded
 var welcomeMessages = ["Wonderful Day", "Relaxed Day", "Great day", "Great Weekend", "Good rest of the week"]
+var randomNumber = welcomeMessages[Math.floor(Math.random()* welcomeMessages.length)];
 
 
 
-
-function randomWelcome(arr) {
-for (let i = arr.length - 1; i > 0; i--){
-    const msg = Math.floor(Math.random() * welcomeMessages)
-}
+/*function randomWelcome(arr) {
+for (let i = arr.length; i > 0; i--){
+    const msg = Math.floor(Math.random() * welcomeMessages[i])
+}return arr
     
-}
-console.log('I Hope You have a ' + welcomeMessages)
+}*/
+console.log('I Hope You have a ' + randomNumber)
 
-
-console.log(helloworld)
 
