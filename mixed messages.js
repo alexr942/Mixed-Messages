6308b701ee7194fa19cc200d466d428acfd9148d
alexr942 <alexrobertson518@gmail.com>
2020-12-07 15:29:58 +0000

    commit "hi"

---
+++ mixed messages.js
@@ -0,0 +1,17 @@
+// this application will show random messages everytime it is loaded
+var welcomeMessages = ["Wonderful Day", "Relaxed Day", "Great day", "Great Weekend", "Good rest of the week"]
+
+
+
+
+function randomWelcome(arr) {
+for (let i = arr.length - 1; i > 0; i--){
+    const msg = Math.floor(Math.random() * welcomeMessages)
+}
+    
+}
+console.log('I Hope You have a ' + welcomeMessages)
+
+
+console.log(helloworld)
+
